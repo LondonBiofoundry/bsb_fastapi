26f2adbeb5f53f1f8a5cbf9664b3c65402ef1f71
Benedict Carling <53439684+Benedict-Carling@users.noreply.github.com>
2021-08-30 17:32:42 +0100

    refactor(buildCSVS): Add execption

--- app/src/buildCSVs.py
+++ app/src/buildCSVs.py
@@ -2,11 +2,16 @@ from typing import List
 from app.schema import basicBuild
 from app.utils.returnBasicBuild import return_build
 from fastapi.responses import FileResponse
+from fastapi import HTTPException
 
 from app.utils.readReturnDelete import read_return_delete
 
 
 def buildCSVs(myBuild: List[basicBuild]):
-    build = return_build(myBuild)
-    filepath = build.export_csvs()
-    return read_return_delete(filepath, "application/zip", "archive.zip")
+    try:
+        build = return_build(myBuild)
+        filepath = build.export_csvs()
+        return read_return_delete(filepath, "application/zip", "archive.zip")
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
